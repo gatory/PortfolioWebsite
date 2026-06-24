93d501afe273c050101a3ae01bdf5120d5ee8726
kuan <rueikuanwei@gmail.com>
2026-06-24 16:59:08 -0700

    feat: added director's note to test out layout

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -30,6 +30,10 @@ export const projects: Project[] = [
             '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene1.png',
             '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene2.png',
         ],
+        directorNote: `A reliable digital desktop timer built to help users beat procrastination 
+        and stay focused using the Pomodoro study method, which cycles between 
+        dedicated work intervals and short breaks. Targeted to students and helps 
+        boost productivity.`
     },
     {
         id: "full-stack",
@@ -64,7 +68,7 @@ export const projects: Project[] = [
         featured: true,
         currentlyBuilding: true,
         projectName: "embedded",
-        route: "/2-data",
+        route: "/projects/2",
         githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
         background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
         status: "completed",
@@ -93,7 +97,7 @@ export const projects: Project[] = [
         featured: true,
         currentlyBuilding: true,
         projectName: "ai / machine-learning",
-        route: "/3-data",
+        route: "/projects/3",
         githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
         background: "",
         status: "completed",
@@ -122,7 +126,7 @@ export const projects: Project[] = [
         featured: true,
         currentlyBuilding: false,
         projectName: "Data",
-        route: "/4-data",
+        route: "/projects/4",
         githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
         background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
         status: "completed",
@@ -150,7 +154,7 @@ export const projects: Project[] = [
         featured: true,
         currentlyBuilding: true,
         projectName: "Portfolio Website",
-        route: "/5-portfolio-website",
+        route: "/projects/5",
         githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
         background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
         status: "completed",
@@ -179,7 +183,7 @@ export const projects: Project[] = [
         featured: true,
         currentlyBuilding: false,
         projectName: "Data",
-        route: "/6-data",
+        route: "/projects/6",
         githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
         background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
         status: "completed",
@@ -206,7 +210,88 @@ export const projects: Project[] = [
         featured: true,
         currentlyBuilding: false,
         projectName: "Data",
-        route: "/7-data",
+        route: "/projects/7",
+        githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
+        background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
+        status: "completed",
+        icon: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-icon.png",
+
+        overview: `A reliable digital desktop timer built to help users beat procrastination 
+        and stay focused using the Pomodoro study method, which cycles between 
+        dedicated work intervals and short breaks. Targeted to students and helps 
+        boost productivity.`,
+        runtime: "4 Months",
+        description: `A desktop Pomodoro study timer built for students to beat procrastination.`,
+        cast: [
+            { name: 'Python', role: 'Data Analysis' },
+            { name: 'MySQL', role: 'Database' },
+        ],
+        scenes: [
+            '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene1.png',
+            '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene2.png',
+        ],
+    },
+    {
+        id: "8",
+        category: "data",
+        featured: true,
+        currentlyBuilding: false,
+        projectName: "Data",
+        route: "/projects/8",
+        githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
+        background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
+        status: "completed",
+        icon: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-icon.png",
+
+        overview: `A reliable digital desktop timer built to help users beat procrastination 
+        and stay focused using the Pomodoro study method, which cycles between 
+        dedicated work intervals and short breaks. Targeted to students and helps 
+        boost productivity.`,
+        runtime: "4 Months",
+        description: `A desktop Pomodoro study timer built for students to beat procrastination.`,
+        cast: [
+            { name: 'Python', role: 'Data Analysis' },
+            { name: 'MySQL', role: 'Database' },
+        ],
+        scenes: [
+            '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene1.png',
+            '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene2.png',
+        ],
+    },
+    {
+        id: "9",
+        category: "data",
+        featured: true,
+        currentlyBuilding: false,
+        projectName: "Data",
+        route: "/projects/9",
+        githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
+        background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
+        status: "completed",
+        icon: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-icon.png",
+
+        overview: `A reliable digital desktop timer built to help users beat procrastination 
+        and stay focused using the Pomodoro study method, which cycles between 
+        dedicated work intervals and short breaks. Targeted to students and helps 
+        boost productivity.`,
+        runtime: "4 Months",
+        description: `A desktop Pomodoro study timer built for students to beat procrastination.`,
+        cast: [
+            { name: 'Python', role: 'Data Analysis' },
+            { name: 'MySQL', role: 'Database' },
+        ],
+        scenes: [
+            '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene1.png',
+            '/images/Eggcellent-Pomodoro/eggcellent-pomodoro-scene2.png',
+        ],
+    },
+    {
+        id: "10",
+        category: "data",
+        featured: true,
+        currentlyBuilding: false,
+        projectName: "Data",
+        route: "/projects/10",
         githubUrl: "https://github.com/gatory/Eggcellent-Pomodoro.git",
         background: "/images/Eggcellent-Pomodoro/eggcellent-pomodoro-bg.png",
         status: "completed",
